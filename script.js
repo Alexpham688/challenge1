f207daf967763917fa791580922c3e2627e9bc56
Alexpham688 <Alexpham688@gmail.com>
2017-04-13 22:16:53 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,36 @@
+var farhenheit =70;
+
+var celcius = (farhenheit - 32)  / 1.8;
+
+console.log(y);
+console.log( 'farhenheit temp' + 'converted temp' = 'celcius temp');
+
+var y = 21.1
+
+var x = ( y * 1.8) + 32;
+
+console.log(farhenheit);
+console.log( 'celcius temp' * 1.8 + 32);
+
+
+var casual = temp;
+var temp = 50;
+if ( temp >= 53){
+  alert("wear something light");
+} else if (temp < 53) {
+  alert("wear a jacket");
+}
+var formal = temp;
+var temp = 60;
+if  (temp >= 70){
+  alert("wear a suit and tie");
+} else if (temp < 70);{
+  alert("wear a trench coat");
+}
+var semiformal = temp;
+var temp = 35;
+if (temp >=34) {
+  alert("wear a blazer")
+}else if (temp < 34){
+  alert("wear a coat");
+}
